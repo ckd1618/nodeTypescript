86673cd4010bd3ed50cb812ef1831d60a9781aa6
Darakjian <cdarakjian@deloitte.com>
2020-05-03 22:02:35 -0700

    added algtest folder

---
+++ instructions/algtest/algtest.ts
@@ -0,0 +1,3 @@
+var x: number = 5;
+console.log(x**2);
+console.log("hi");
---
+++ src/app.ts
@@ -0,0 +1,6 @@
+var x: string = "hello";
+var y: number = 144;
+console.log(x);
+console.log(y**2);
+console.log(x, y);
+console.log("next8");
